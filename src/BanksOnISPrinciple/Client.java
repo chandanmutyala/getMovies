e67b199170936113f59918667475ec1e9c1a236d
chandanmutyala <chandanmutyala@gmail.com>
2023-12-12 15:12:46 +0530

    intial commit

--- src/BanksOnISPrinciple/Client.java
+++ src/BanksOnISPrinciple/Client.java
@@ -2,13 +2,11 @@ package BanksOnISPrinciple;
 
 public class Client {
     public static void main(String [] args){
-   CityBank city =new CityBank();
-    //city.addBeneficiary(678489379,"Siva");
-    //city.transfer(678489379,500);
+
 
     HdfcBank hdfc =new HdfcBank();
-
-     hdfc.otpValidation(669,"hdfcclient@123");
+         hdfc.addBeneficiary(87873,"Siva");
+         hdfc.otpValidation(6669,"hdfcclient@123");
          hdfc.transfer(878738,87);
 
 
